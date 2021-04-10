81b1074e7e6141c22f4373db468bb038bd87c036
Juan Gallardo <josejuangallardocabrera@gmail.com>
2021-04-10 17:12:24 -0500

    Algoritmo Ordenamiento Burbuja terminado

--- OrdenamientoBurbuja.java
+++ OrdenamientoBurbuja.java
@@ -1,5 +1,23 @@
 
-
 public class OrdenamientoBurbuja {
+    public static void main(String[] args){
 
+        int[] datos = {2, 3, 1, 5, 4};
+        int aux;
+
+        for(int i=0; i < datos.length-1; i++){
+            for(int j=0; j < datos.length-1; j++){
+                if(datos[j] > datos[j+1]){
+                    aux = datos[j];
+                    datos[j] = datos[j+1];
+                    datos[j+1] = aux;
+                }
+            }
+        }
+
+        System.out.println("Arreglo Ordenado");
+        for(int i=0; i < datos.length; i++){  
+            System.out.println(datos[i]);
+        }
+    }
 }
