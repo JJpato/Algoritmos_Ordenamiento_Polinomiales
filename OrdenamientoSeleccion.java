73bd71944777a50d4e3e61f183a534ae02d75269
Juan Gallardo <josejuangallardocabrera@gmail.com>
2021-04-12 23:56:06 -0500

    Algoritmo de Ordenamiento por seleccion terminado

--- OrdenamientoSeleccion.java
+++ OrdenamientoSeleccion.java
@@ -1,4 +1,26 @@
 
 public class OrdenamientoSeleccion {
+    public static void main(String[] args){
+        int[] datos = {2, 3, 1, 5, 4, 6};
+        int aux, min;
+    
+        for(int i=0; i<datos.length-1; i++){
+            min = i; 
+            //aux = datos[i]; 
 
+            for(int j = i+1; j < datos.length; j++){
+                if(datos[j] < datos[min]){
+                    min = j;
+                }
+            }
+            aux = datos[i];
+            datos[i] = datos[min];
+            datos[min] = aux;
+        }
+
+        System.out.println("Arreglo Ordenado");
+        for(int i=0; i < datos.length; i++){  
+            System.out.println(datos[i]);
+        }
+    }
 }
